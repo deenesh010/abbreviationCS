59cb025143345ee201ce2a2061a1f610ac5c358a
deenesh010 <dinesh.shrestha2001@gmail.com>
2023-05-24 21:55:21 +0545

    C# lab 1.15

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tmp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Amit Kumar Sharma";
+            System.Console.WriteLine(name);
+            string[] words=name.Split(' ');
+           /* foreach(string word in words)
+            {
+                System.Console.WriteLine($"{word}");
+            }*/
+            string abbr = words[0].Substring(0,1)+"."+ words[1].Substring(0, 1) +"."+ words[2];
+            Console.WriteLine(abbr);
+        }
+    }
+}
